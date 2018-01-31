8a557b67cba5f8fa8fe0b54edeb1b65c9b494448
root <EldonZhao>
2018-01-31 07:49:28 +0000

    cecelery tests

--- celery/tasks.py
+++ celery/tasks.py
@@ -1,9 +1,19 @@
 from celery import Celery
+import time
 
 
-app = Celery('tasks', broker='amqp://guest@localhost//')
+app = Celery('tasks', broker='amqp://guest@localhost//', backend='amqp://guest@localhost//')
 
 
 @app.task
 def add(x, y):
     return x + y
+
+
+@app.task
+def mul(x):
+    result = 0
+    for i in xrange(x):
+        time.sleep(x)
+        result += i
+    return result
